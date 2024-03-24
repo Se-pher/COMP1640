d3a019df8012f4ac7bf506898a436315d601882b
Tannguyen29 <tannvgdh210106@fpt.edu.vn>
2024-03-24 09:47:55 +0700

    Articles Main Page

--- frontend/src/Components/LandingPage.jsx
+++ frontend/src/Components/LandingPage.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Mountain from "../Image/Mountains.jpg";
 import * as s from "../Style/Landing";
+import ArticleCard from "./ArticleCard";
+import { articles } from "./data";
 
 const LandingPage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const articlesPerPage = 9;
+  const indexOfLastArticle = currentPage * articlesPerPage;
+  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
+  const currentArticles = articles.slice(indexOfFirstArticle, indexOfLastArticle);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <s.Container>
       <s.Header>
@@ -15,6 +27,22 @@ const LandingPage = () => {
       </s.Header>
       <s.Section>
         <s.Title>Articles</s.Title>
+        <s.ArticleGrid>
+          {currentArticles.map((article) => (
+            <ArticleCard key={article.id} article={article} />
+          ))}
+        </s.ArticleGrid>
+        <s.Pagination>
+          {Array.from({ length: Math.ceil(articles.length / articlesPerPage) }, (_, i) => (
+            <button
+              key={i + 1}
+              onClick={() => handlePageChange(i + 1)}
+              disabled={currentPage === i + 1}
+            >
+              {i + 1}
+            </button>
+          ))}
+        </s.Pagination>
       </s.Section>
     </s.Container>
   );
