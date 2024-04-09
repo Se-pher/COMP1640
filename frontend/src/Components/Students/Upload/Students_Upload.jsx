171269ae84f3f97efcfcf660db8742ccfe9d4e37
Tannguyen29 <tannvgdh210106@fpt.edu.vn>
2024-04-09 11:41:03 +0700

    student Style responsive

--- frontend/src/Components/Students/Upload/Students_Upload.jsx
+++ frontend/src/Components/Students/Upload/Students_Upload.jsx
@@ -272,7 +272,7 @@ const Student_Upload = () => {
               <s.TitleHeader>Upload Articles</s.TitleHeader>
               <s.InputWrapper>
                 <s.Label>Select Faculty:</s.Label>
-                <select value={facultyName} onChange={handleFacultyChange}>
+                <s.setFaculty value={facultyName} onChange={handleFacultyChange}>
                   <option value="">Select Faculty</option>
                   {facultyList.map((faculty) => (
                     <option
@@ -288,7 +288,7 @@ const Student_Upload = () => {
                       )}
                     </option>
                   ))}
-                </select>
+                </s.setFaculty>
               </s.InputWrapper>
               <s.InputWrapper>
                 <s.Label>Title:</s.Label>
